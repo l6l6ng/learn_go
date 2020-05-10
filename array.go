975069a0cb5684597c57057be6542e87753783c0
dfsd534 <dfsd534@qq.com>
2020-05-10 14:15:05 +0800

    go

---
+++ array.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+var ary = [...] float32 {1, 2, 3, 4, 5, 6}
+func main() {
+	a := ary[2]
+	fmt.Println(a)
+}
---
+++ ud.go
@@ -0,0 +1,11 @@
+type ages int
+
+type money float32
+
+type months map[string]int 
+
+m := months {
+	"january":31,
+	"februay":28,
+	"december":31,
+}
