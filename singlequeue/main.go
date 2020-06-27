d6eec9a0346d1be57e0bcf6af94064ede506f40a
dfsd534 <dfsd534@qq.com>
2020-06-27 10:23:36 +0800

    test

--- singlequeue/main.go
+++ singlequeue/main.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Queue struct {
-	maxSize 4
-	array [4]int
+	maxSize int
+	array [5]int
 	front int
 	rear int
 }
@@ -25,5 +25,17 @@ func (this *Queue) AddQueue(val int) (error) {
 
 //显示队列
 func (this *Queue) ShowQueue() {
-	
+	//this.front 不包含队首的元素
+	for i := this.front + 1;i<=this.rear;i++{
+		fmt.Printf("array[%d]=%d\t", i, this.array[i])
+	}
+}
+
+//编写一个主函数测试，
+
+func main(){
+	queue = Queue{
+		maxSize: 5,
+		
+	}
 }
